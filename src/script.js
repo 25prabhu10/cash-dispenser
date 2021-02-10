959ec36e7bfa35fd0ad22d4de58d84fa315b1c26
Prabhu K H <25prabhu10@gmail.com>
2021-02-10 21:46:31 +0530

    feat: Add error messages

--- src/script.js
+++ src/script.js
@@ -1,13 +1,25 @@
 window.addEventListener('load', () => {
   const dispatchBtn = document.getElementById('dispatch-btn');
   const inputAmount = document.getElementById('amount');
+  const noteCount = document.getElementById('note-count');
+  const errMsg = document.getElementById('err-msg');
 
   const updateCount = () => {
-    let dispatch = parseInt(inputAmount.value);
+    let dispatch = parseInt(inputAmount.value, 10);
 
     // IF VALUE IS 0 OR EMPTY THEN DON't PROCEED
-    if (dispatch === 0 || isNaN(dispatch)) {
+    errMsg.classList.remove('hide');
+    errMsg.classList.add('danger');
+    if (isNaN(dispatch)) {
+      errMsg.textContent = 'Please only numbers';
       return;
+    } else if (dispatch <= 0 || dispatch > 999999999999999999) {
+      errMsg.textContent =
+        'Please enter amount between ₹1 to ₹999999999999999999';
+      return;
+    } else {
+      errMsg.classList.remove('danger');
+      errMsg.classList.add('hide');
     }
 
     const denominations = [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
@@ -25,6 +37,8 @@ window.addEventListener('load', () => {
     ];
 
     let total = 0;
+
+    noteCount.textContent = `Note Count for Amount ₹${dispatch}`;
 
     for (let counter = 0; counter < names.length; counter++) {
       let count = 0;
@@ -44,7 +58,7 @@ window.addEventListener('load', () => {
     }
 
     document.getElementById('total').textContent = total;
-    document.getElementById('note-count').scrollIntoView();
+    noteCount.scrollIntoView();
     inputAmount.value = '';
   };
 
